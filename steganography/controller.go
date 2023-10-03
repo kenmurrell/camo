b76f7f621aea94de9eda6ebfafe20b5e2287110f
kenmurrell <ken.murrell13@gmail.com>
2023-10-03 17:33:10 -0400

    encryption_option: added encryption option

--- steganography/controller.go
+++ steganography/controller.go
@@ -8,14 +8,19 @@ import (
 	"os"
 )
 
-type Mode byte
+type RunOptions struct{
+	Mode EncodingMode
+	Encrypt bool
+}
+
+type EncodingMode byte
 
 const (
-	BlueRGBA Mode = iota
-	AllRGBA Mode = iota
+	BlueRGBA EncodingMode = iota
+	AllRGBA EncodingMode = iota
 )
 
-func Encode(hostFile *os.File, hideFile *os.File, hostedFile *os.File, m Mode) error {
+func Encode(hostFile *os.File, hideFile *os.File, hostedFile *os.File, r RunOptions) error {
 	fmt.Print("Encoding...")
 	hostIm, _, err := image.Decode(hostFile)
 	if err != nil {
@@ -24,8 +29,13 @@ func Encode(hostFile *os.File, hideFile *os.File, hostedFile *os.File, m Mode) e
 	fi, _ := hideFile.Stat()
 	data := make([]byte, fi.Size())
 	hideFile.Read(data)
+	if r.Encrypt{
+		encryptor := Encryptor{}
+		if data, err = encryptor.Encrypt(data); err != nil{
+			return fmt.Errorf("ERROR: image cannot be encrypted %s", err.Error())
+		}
+	}
 	bounds := hostIm.Bounds()
-
 	capacity := int((bounds.Max.Y - bounds.Min.Y) * (bounds.Max.X - bounds.Min.X) / 8)
 	if len(data) > capacity {
 		return fmt.Errorf("ERROR: ghost file exceeds host file's capacity (>%d bytes)", capacity)
@@ -33,7 +43,7 @@ func Encode(hostFile *os.File, hideFile *os.File, hostedFile *os.File, m Mode) e
 	if hostImNRGBA, ok := hostIm.(*image.NRGBA); ok {
 		var newImg *image.NRGBA
 		var nencoded int 
-		if m == AllRGBA {
+		if r.Mode == AllRGBA {
 			encoder := nrbgaAllEncoder{hostImNRGBA}
 			newImg, nencoded = encoder.encode(data)
 		}else{
@@ -52,7 +62,7 @@ func Encode(hostFile *os.File, hideFile *os.File, hostedFile *os.File, m Mode) e
 	return nil
 }
 
-func Decode(hostFile *os.File, outputFile *os.File, m Mode) error {
+func Decode(hostFile *os.File, outputFile *os.File, r RunOptions) error {
 	fmt.Print("Decoding...")
 	hostIm, _, err := image.Decode(hostFile)
 	if err != nil {
@@ -62,12 +72,18 @@ func Decode(hostFile *os.File, outputFile *os.File, m Mode) error {
 	capacity := int((bounds.Max.Y - bounds.Min.Y) * (bounds.Max.X - bounds.Min.X) / 2)
 	data := make([]byte, capacity)
 	if hostImNRGBA, ok := hostIm.(*image.NRGBA); ok {
-		if m == AllRGBA {
+		if r.Mode == AllRGBA {
 			encoder := nrbgaAllEncoder{hostImNRGBA}
 			_ = encoder.decode(data)
 		}else{
 			encoder := nrbgaBlueEncoder{hostImNRGBA}
 			_ = encoder.decode(data)
+		}
+	}
+	if r.Encrypt{
+		encryptor := Encryptor{}
+		if data, err = encryptor.Decrypt(data); err != nil{
+			return fmt.Errorf("ERROR: image cannot be decrypted %s", err.Error())
 		}
 	}
 	if _, err = outputFile.Write(data); err != nil {
